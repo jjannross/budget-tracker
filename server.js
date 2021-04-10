8e55228083152a604a14d9ec6c2a3119d2ccdbc0
Jennifer Ross <jjannross@gmail.com>
2021-04-10 14:13:26 -0400

    added link to readme

--- server.js
+++ server.js
@@ -20,6 +20,16 @@ mongoose.connect("mongodb://localhost/budget", {
   useFindAndModify: false
 });
 
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/budget',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
 // routes
 app.use(require("./routes/api.js"));
 
